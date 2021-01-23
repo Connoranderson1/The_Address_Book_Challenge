9cb32224e815c49fd7901e0eee8080c369bc4d69
Trent Connor Anderson <trentconnoranderson@Esmeraldas-MacBook-Air.local>
2021-01-23 00:29:06 -0500

    Added one more test and coded out HTML

--- cypress/integration/userCanCreateAContact.feature.js
+++ cypress/integration/userCanCreateAContact.feature.js
@@ -2,5 +2,11 @@
 describe('user can create a contact', () => {
 	it('test', () => {
 		cy.visit('http://localhost:3001')
-	})
+    })
+    describe('user can create a contact', () => {
+        it('test', () => {
+            cy.visit('http://localhost:3001')
+            cy.get('#add-contact').click()
+        })
+    })
 })
